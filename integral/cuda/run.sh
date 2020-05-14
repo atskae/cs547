d4b3f4928bfe9a9531c1d6f78b41b7ff477c723f
atskae <atskae@gmail.com>
2020-05-14 09:53:04 -0400

    Minor error in run script

--- integral/cuda/run.sh
+++ integral/cuda/run.sh
@@ -4,7 +4,7 @@
 
 if [ $# != 4 ] # if number of arguments != 4
 then
-    echo "./integrate lowerLimit upperLimit numSamples maxNumThreads"
+    echo "./integrate lowerLimit upperLimit numSamples numNumThreads"
     exit 1
 fi
 
@@ -14,21 +14,11 @@ make
 A=$1
 B=$2
 NUM_SAMPLES=$3
-MAX_THREADS=$4
-echo "a=$A,b=$B,numSamples=$NUM_SAMPLES,maxThreads=$MAX_THREADS"
+NUM_THREADS=$4
+echo "a=$A,b=$B,numSamples=$NUM_SAMPLES,numThreads=$NUM_THREADS"
 
 ## Run integrate
 srun ./integrate $A $B $NUM_SAMPLES $NUM_THREADS
 
 ## Clean executable
-#make clean
-#
-## Take absolute value for naming purposes
-#ABS_1=$1
-#ABS_2=$2
-#
-#DATA_DIR="$ABS_1-$ABS_2-$3-all"
-#rm -rf $DATA_DIR
-#mkdir $DATA_DIR
-#mv *.csv $DATA_DIR
-#tar -czvf "$DATA_DIR.tar.gz" $DATA_DIR
+make clean
